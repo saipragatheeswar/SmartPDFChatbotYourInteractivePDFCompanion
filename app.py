f8dd904532a578e397f263a7cfe2b8c640ac267f
saipragatheeswar <111628417+saipragatheeswar@users.noreply.github.com>
2024-09-18 12:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-import streamlit as st
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chains import create_history_aware_retriever, create_retrieval_chain
+from langchain_chains.combine_documents import create_stuff_documents_chain
+from langchain.vectorstores import Chroma  # Updated import
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_groq import ChatGroq
-from langchain.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
